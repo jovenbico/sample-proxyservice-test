078864cf412eb42f9936c099b274faab0922b296
jovenbico <jovenbico@gmail.com>
2018-05-22 23:51:56 +0800

    try creating test suite

--- src/test/java/com/bicjo/sample/ProxyServiceApplicationTests.java
+++ src/test/java/com/bicjo/sample/ProxyServiceApplicationTests.java
@@ -5,6 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({ //
+//		ApplicationConfigurationTest.class, //
+//		CustomerTest.class, //
+//		CuntomerControllerTest.class //
+//})
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProxyServiceApplicationTests {
